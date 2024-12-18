d84559f777db752ee4d60e58d706f7cd8c7392d6
abdullahss <abdullh2004ss@gmail.com>
2024-12-18 14:27:59 +0200

    fix type bug

--- app/(grid)/game/[id]/page.tsx
+++ app/(grid)/game/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import React from "react";
 
 const page = async ({ params }: { params: { id: string } }) => {
-  const { id } = await params;
+  const { id } = params;
   const game = await getGame(id);
   console.log(game);
   const { screenshots, data, similar }: { screenshots: any[]; data: any; similar: any[] } = game;
